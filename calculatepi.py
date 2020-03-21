89b015bcdbd7f400319ec0224bf1d9388869cc63
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-03-21 14:46:29 -0700

    Add files via upload
    reorganized files to be in playground repository

---
+++ calculatepi.py
@@ -0,0 +1,15 @@
+#estimate pi, given random points between 0-1
+import random
+
+def estimate_pi(n):
+    num_point_circle = 0
+    num_point_total = 0
+    for _ in range(n):
+        x = random.unifrom (0,1)
+        y = random.unifrom (0,1)
+        distance = x**2 + y**2
+        if distance <= 1:
+            num_point_circle += 1
+        num_point_total += 1
+
+    return 4 * num_point_circle/num_point_total
