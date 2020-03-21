3809205a8a4c9a4af79efc2e4c0542882971a0d6
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-03-21 16:48:57 -0700

    Update calculatepi.py
    just fixed syntax for random.uniform

--- calculatepi.py
+++ calculatepi.py
@@ -5,8 +5,8 @@ def estimate_pi(n):
     num_point_circle = 0
     num_point_total = 0
     for _ in range(n):
-        x = random.unifrom (0,1)
-        y = random.unifrom (0,1)
+        x = random.uniform (0,1)
+        y = random.uniform (0,1)
         distance = x**2 + y**2
         if distance <= 1:
             num_point_circle += 1
