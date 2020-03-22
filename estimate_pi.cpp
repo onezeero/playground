612b8802b61ccb012b34e4e56adb83d38ee40079
onezeero <44596978+onezeero@users.noreply.github.com>
2020-03-21 17:18:46 -0700

    Update estimate_pi.cpp

--- estimate_pi.cpp
+++ estimate_pi.cpp
@@ -1,21 +1,19 @@
-#include<random>
 #include<iostream>
 #include<cmath>
+using namespace std;
 
-int main()
-{
-  double estimate_pi(n);
+int main() {
   double num_point_circle = 0;
   double num_point_total = 0;
-  for (n) {
-    double x = random.unifrom (0,1);
-    double y = random.unifrom (0,1);
-    double distance = x**2 + y**2;
+  while(true) {
+	double x = ((double) rand() / (RAND_MAX));
+	double y = ((double) rand() / (RAND_MAX));
+    double distance = x*x+y*y;
         if (distance <= 1){
             num_point_circle += 1;
-            num_point_total += 1;
           }
+        num_point_total += 1;
     double pi = 4 * num_point_circle/num_point_total;
-    return pi
+    cout<<pi<<endl;
   }
 }
